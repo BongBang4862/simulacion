273d2af6102ef2b29e07a6ead52f00cf430367fb
BongBang <luis.cordova.olavarria@gmail.com>
2023-04-03 12:38:58 -0400

    resolviendo lo de la reserva de simuladores

--- calendar.php
+++ calendar.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,43 +10,14 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.2/fullcalendar.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.2/locale/es.min.js"></script>
+    <script src="js/calendar.js" type="text/javascript"></script>
 </head>
 <body>
+    <?php
+        include ('templates/modal.php');
+    ?>
     <div id="calendar"></div>
 
-    <script>
-        $(document).ready(function() {
-            // Obtener eventos desde el servidor
-            $.ajax({
-                url: "consultas/eventos.php",
-                type: "GET",
-                success: function(response) {
-                    var eventos = [];
-                    response.forEach(function(evento) {
-                        eventos.push({
-                            title: evento.nombre_simulador,
-                            start: evento.fecha + 'T' + evento.hora_inicio,
-                            end: evento.fecha + 'T' + evento.hora_final,
-                            color: evento.color
-                        });
-                    });
-
-                    // Inicializar el calendario
-                    $('#calendar').fullCalendar({
-                        locale: 'es',
-                        header: {
-                            left: 'prev,next today',
-                            center: 'title',
-                            right: 'month,agendaWeek,agendaDay'
-                        },
-                        navLinks: true,
-                        editable: false,
-                        eventLimit: true,
-                        events: eventos
-                    });
-                }
-            });
-        });
-    </script>
+    
 </body>
 </html>
